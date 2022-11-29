5cb4a2f83bf5ee0267273cfed3aabd6081cb18ba
zachbaker55 <thezachbaker55@gmail.com>
2022-11-29 17:31:28 -0500

    project.godot and basic menus added

---
+++ Global.gd
@@ -0,0 +1,22 @@
+extends Node
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
+func _unhandled_input(event):
+	if event.is_action_pressed("menu"):
+		var Pause_Menu = get_node_or_null("/root/Game/UI/Pause_Menu")
+		if Pause_Menu == null:
+			get_tree().quit()
+		else:
+			if Pause_Menu.visible:
+				get_tree().paused = false
+				Pause_Menu.hide()
+			else:
+				get_tree().paused = true 
+				Pause_Menu.show()
+	
+
+		
+func reset():
+	var _scene = get_tree().change_scene("res://Game.tscn")
